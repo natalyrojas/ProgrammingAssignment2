a2cec72e6681cb054a6439521446894ca6813fcb
Nataly <rojas.nataly@gmail.com>
2015-02-22 09:42:34 -0430

    Tarea 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Apply the solve funtions to a matrix in cache
+## 
 
-## Write a short comment describing this function
+## Create a list with four elements, a setter and getter of a function to apply 
+## and the getter and setter of the  result of applying the function
+ 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get, setsolve = setsolve,getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Apply the function SOLVE to a matrix in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
